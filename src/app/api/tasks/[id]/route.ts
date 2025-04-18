000fea00cdbc199c2f16abb69820eeedc52e6264
Kunal Patil <kunalpatil970730@gmail.com>
2025-04-18 23:32:40 +0530

    fix: fix added

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,7 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
-export async function PATCH(req: NextRequest, { params }: { params: { id: string } }){
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+){
   const { id } = await params;
   console.log(id);
   if (!id) {
